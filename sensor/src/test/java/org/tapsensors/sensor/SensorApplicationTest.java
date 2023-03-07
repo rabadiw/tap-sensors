5717b1d5537e9dae7c796512efd417d00bc67428
Dariusz Lorenc <dlorenc@vmware.com>
2023-03-06 17:30:19 -0700

    Disable spring boot test for now
    as we don't have services (testcontainers) to be able to run those tests

--- sensor/src/test/java/org/tapsensors/sensor/SensorApplicationTest.java
+++ sensor/src/test/java/org/tapsensors/sensor/SensorApplicationTest.java
@@ -1,9 +1,11 @@
 package org.tapsensors.sensor;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled
 class SensorApplicationTest {
 
 	@Test
